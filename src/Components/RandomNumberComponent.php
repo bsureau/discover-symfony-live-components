3c899b1871fb55ce94678418472781fee8197cf8
bsureau <sureau.benjamin@gmail.com>
2022-04-02 14:31:49 +0000

    feat: actions and arguments

--- src/Components/RandomNumberComponent.php
+++ src/Components/RandomNumberComponent.php
@@ -2,11 +2,12 @@
 
 namespace App\Components;
 
+use Psr\Log\LoggerInterface;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-
-
 #[AsLiveComponent('random_number')]
 class RandomNumberComponent
 {
@@ -21,5 +22,13 @@ class RandomNumberComponent
     public function getRandomNumber(): int
     {
         return rand($this->min, $this->max);
+    }
+
+    #[LiveAction]
+    public function resetMinMax(LoggerInterface $logger, #[LiveArg('min')] int $min, #[LiveArg('max')] int $max)
+    {
+        $this->min = $min;
+        $this->max = $max;
+        $logger->info("Min/Max were reset!");
     }
 }
